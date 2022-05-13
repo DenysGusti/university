1d197b5fc588dfcdc1a30eeacadbed623e303c4e
Denys Gusti <90290236+DenysGusti@users.noreply.github.com>
2022-05-13 12:18:33 +0300

    updated

--- _Practise/task_9_5.cpp
+++ _Practise/task_9_5.cpp
@@ -12,31 +12,21 @@
 #include "task_9_5.h"
 
 int main() {
-    DoublyLinkedList<int> arr;
-    for (int i = 0; i < 35'000'000; ++i)
-        arr.push_back(i);
-    cout << "good" << endl;
-    auto start = chrono::steady_clock::now();
-    auto p = arr.at(17'599'998);
-    cout << p;
-    auto end = chrono::steady_clock::now();
-    chrono::duration<double> time = end - start;
-    cout << endl << time.count() << endl;
-//    bool input = false; // відповідає за те, чи ввід з клавіатури, чи з файлу
-//    Stack<int> stack1, stack2; // власний стек на основі двонапрямного списку
-//    fstream fs{"task_9_5.txt"};
-//    // якщо ввід з клавіатури, peek дивиться наступний символ, але не переміщується, на відміну від get
-//    for (int i = 1, c; input && cin.peek() != '\n' && cin >> c || !input && fs >> c; ++i)
-//        (i % 3 ? stack2 : stack1).push(c); // якщо позиція ділиться на 3, пушу в перший стек, інакше в другий
-//
-//    while (!stack1.empty()) { // виводжу перший стек (last in first out - виводиться задом наперед)
-//        cout << stack1.top() << ' ';
-//        stack1.pop();
-//    }
-//
-//    while (!stack2.empty()) { // виводжу другий стек
-//        cout << stack2.top() << ' ';
-//        stack2.pop();
-//    }
+    bool input = false; // відповідає за те, чи ввід з клавіатури, чи з файлу
+    Stack<int> stack1, stack2; // власний стек на основі двонапрямного списку
+    fstream fs{"task_9_5.txt"};
+    // якщо ввід з клавіатури, peek дивиться наступний символ, але не переміщується, на відміну від get
+    for (int i = 1, c; input && cin.peek() != '\n' && cin >> c || !input && fs >> c; ++i)
+        (i % 3 ? stack2 : stack1).push(c); // якщо позиція ділиться на 3, пушу в перший стек, інакше в другий
+
+    while (!stack1.empty()) { // виводжу перший стек (last in first out - виводиться задом наперед)
+        cout << stack1.top() << ' ';
+        stack1.pop();
+    }
+
+    while (!stack2.empty()) { // виводжу другий стек
+        cout << stack2.top() << ' ';
+        stack2.pop();
+    }
     return 0;
 }
